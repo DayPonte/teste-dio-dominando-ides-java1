7a9cb3aeebdebce7ec7fcd0a90f4f55c4404a65f
Dayane <dayponte@gmail.com>
2022-07-19 20:29:32 -0300

    first commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package com.dio;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		int a = 2;
+		int b = 3;
+		System.out.println("Hello World! " + (a+b));
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
