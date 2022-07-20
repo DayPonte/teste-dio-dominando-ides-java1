e88cc31ed92974d528228e7545a13d4c96e8f88b
Dayane <dayponte@gmail.com>
2022-07-20 14:48:38 -0300

    criação de calculadora

---
+++ src/com/dio/calculadora/Calculadora.java
@@ -0,0 +1,58 @@
+package com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		try (Scanner scan = new Scanner(System.in)) {
+			int a, b;
+	
+			public class Hex {
+				  public void main(String args[a.b]){
+				    int i = 42;
+				    
+		
+			
+			System.out.println("Digite o primeiro valor: ");
+			a = scan.nextInt();
+			System.out.println("Digite segundo valor: ");
+			b = scan.nextInt();
+			System.out.println("Digite segundo valor: ");
+			b = scan.nextInt();
+			
+			
+			
+			int soma = soma(a,b);
+			int subtracao = subtraçao(a,b);
+			int multiplicacao = multiplicacao(a,b);
+			int divisao = divisao(a,b);
+			
+			
+			System.out.println(soma);
+			System.out.println(subtracao);
+			System.out.println(multiplicacao);
+			System.out.println(divisao);
+		}
+		
+	}
+		
+
+	public static int soma(int a, int b) {
+		return a + b;
+	}
+	
+	public static int subtraçao(int a, int b) {
+		return a - b;
+	}
+	
+	public static int divisao(int a, int b) {
+		return a / b;
+		
+	}
+	public static int multiplicacao(int a, int b) {
+		return a * b;
+		
+			}
+	
+}
